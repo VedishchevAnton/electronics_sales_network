e210e7a61f4e38569d2aaaf6080e405b4b27113d
VedishchevAnton <vedishchevanton@gmail.com>
2023-10-05 20:36:44 +0300

    тестирование админки фабрика

--- sales_network/admin.py
+++ sales_network/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from sales_network.models.contacts import Contacts
+from sales_network.models.factory import Factory
 
 
 # Register your models here.
@@ -10,3 +11,11 @@ class ContactsAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'phone_number', 'country', 'city', 'street', 'house_number')
     list_filter = ('country', 'city')
     search_fields = ('email', 'phone_number',)
+
+
+@admin.register(Factory)
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'sales_network', 'contacts', 'created_at', 'is_active',)
+    list_filter = ('name', 'sales_network',)
+    search_fields = ('name',)
+
