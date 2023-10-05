402b7574b2234d9fb1392c940806ac9b7420cc34
VedishchevAnton <vedishchevanton@gmail.com>
2023-10-05 21:40:27 +0300

    тест админки ИП

--- sales_network/admin.py
+++ sales_network/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from sales_network.models.contacts import Contacts
 from sales_network.models.factory import Factory
+from sales_network.models.individual_entrepreneur import IndividualEntrepreneur
 from sales_network.models.retail_network import RetailNetwork
 from sales_network.models.sales_network import SalesNetwork
+from sales_network.utils import display_products
 
 
 # Register your models here.
@@ -18,8 +20,8 @@ class ContactsAdmin(admin.ModelAdmin):
 @admin.register(SalesNetwork)
 class SalesNetworkAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'contacts', 'created_at', 'is_active', 'display_products')
-    list_filter = ('name', )
-    search_fields = ('name', )
+    list_filter = ('name',)
+    search_fields = ('name',)
 
     def display_products(self, obj):
         return ", ".join([p.name for p in obj.supplied_products.all()])
@@ -29,9 +31,11 @@ class SalesNetworkAdmin(admin.ModelAdmin):
 
 @admin.register(Factory)
 class FactoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'sales_network', 'contacts', 'created_at', 'is_active', 'display_products')
+    list_display = (
+        'id', 'name', 'sales_network', 'contacts', 'created_at', 'is_active', 'display_products'
+    )
     list_filter = ('name', 'sales_network')
-    search_fields = ('name', )
+    search_fields = ('name',)
 
     def display_products(self, obj):
         return ", ".join([p.name for p in obj.supplied_products.all()])
@@ -52,3 +56,20 @@ class RetailNetworkAdmin(admin.ModelAdmin):
         return ", ".join([p.name for p in obj.supplied_products.all()])
 
     display_products.short_description = 'Продукция торговой сети'
+
+
+@admin.register(IndividualEntrepreneur)
+class IndividualEntrepreneurAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'sales_network', 'contacts', 'retail_network_supplied', 'factory_supplied', 'debt', 'created_at',
+        'is_active', 'display_products'
+    )
+    list_filter = ('name', 'sales_network',)
+    search_fields = ('name',)
+
+    def display_products(self, obj):
+        return ", ".join([p.name for p in obj.supplied_products.all()])
+
+    display_products.short_description = 'Продукция торговой сети'
+
+
