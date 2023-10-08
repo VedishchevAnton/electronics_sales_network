c64f61575e50f42ec628bf9e400288085de21972
VedishchevAnton <vedishchevanton@gmail.com>
2023-10-08 23:57:39 +0300

    реализация запрета обновление через API поля «Задолженность перед поставщиком

--- sales_network/serializers/individual_entrepreneur.py
+++ sales_network/serializers/individual_entrepreneur.py
@@ -4,8 +4,9 @@ from sales_network.models.individual_entrepreneur import IndividualEntrepreneur
 
 
 class IndividualEntrepreneurSerializer(serializers.ModelSerializer):
+    debt = serializers.ReadOnlyField()
+
     class Meta:
         model = IndividualEntrepreneur
         fields = ['id', 'name', 'sales_network', 'contacts', 'supplied_products', 'retail_network_supplied',
                   'factory_supplied', 'debt', 'created_at', 'is_active']
-
