6718d455ce5efd7936fc30ff4720f11eb4dff056
VedishchevAnton <vedishchevanton@gmail.com>
2023-10-08 18:33:24 +0300

    корректировка вывода поставщика у ип

--- sales_network/admin.py
+++ sales_network/admin.py
@@ -60,8 +60,7 @@ class RetailNetworkAdmin(admin.ModelAdmin):
 @admin.register(IndividualEntrepreneur)
 class IndividualEntrepreneurAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'name', 'sales_network', 'contacts', 'retail_network_supplied', 'factory_supplied', 'debt', 'created_at',
-        'is_active', 'display_products'
+        'id', 'name', 'sales_network', 'contacts', 'supplier', 'debt', 'created_at', 'is_active', 'display_products',
     )
     list_filter = ('name', 'sales_network__name', 'contacts__city')
     search_fields = ('name',)
@@ -70,3 +69,21 @@ class IndividualEntrepreneurAdmin(admin.ModelAdmin):
         return ", ".join([p.name for p in obj.supplied_products.all()])
 
     display_products.short_description = 'Продукция торговой сети'
+
+    def supplier(self, obj):
+        # проверяем, выбран ли поставщик оборудования
+        if obj.retail_network_supplied or obj.factory_supplied:
+            # если выбран розничный поставщик, отображаем его название
+            if obj.retail_network_supplied:
+                return obj.retail_network_supplied.name
+            # если выбран завод-поставщик, отображаем его название
+            elif obj.factory_supplied:
+                return obj.factory_supplied.name
+            # если ни один поставщик не выбран, отображаем пустую строку
+            else:
+                return ''
+        # если поставщик не выбран, отображаем пустую строку
+        else:
+            return ''
+
+    supplier.short_description = 'Поставщик оборудования'
