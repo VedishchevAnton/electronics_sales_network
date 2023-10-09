98e6e9115dfd7c4aad8074f5ea5e0f0b433f2e09
VedishchevAnton <vedishchevanton@gmail.com>
2023-10-09 17:51:09 +0300

    реализация CRUD модели Factory

--- sales_network/urls.py
+++ sales_network/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from sales_network.views.factory import FactoryListAPIView, FactoryCreateAPIView, FactoryDetailAPIView, \
+    FactoryUpdateAPIView, FactoryDeleteAPIView
 from sales_network.views.individual_entrepreneur import IndividualEntrepreneurListAPIView, \
     IndividualEntrepreneurCreateAPIView, IndividualEntrepreneurDetailAPIView, IndividualEntrepreneurUpdateAPIView, \
     IndividualEntrepreneurDeleteAPIView
@@ -7,6 +9,12 @@ from sales_network.views.retail_network import RetailNetworkListAPIView, RetailN
     RetailNetworkDetailAPIView, RetailNetworkUpdateAPIView, RetailNetworkDeleteAPIView
 
 urlpatterns = [
+    path('factories/', FactoryListAPIView.as_view(), name='factory-list'),
+    path('factories/create/', FactoryCreateAPIView.as_view(), name='factory-create'),
+    path('factories/<int:pk>/', FactoryDetailAPIView.as_view(), name='factory-detail'),
+    path('factories/<int:pk>/update/', FactoryUpdateAPIView.as_view(), name='factory-update'),
+    path('factories/<int:pk>/delete/', FactoryDeleteAPIView.as_view(), name='factory-delete'),
+
     path('retail-networks/', RetailNetworkListAPIView.as_view(), name='retail-networks_list'),
     path('retail-networks/create/', RetailNetworkCreateAPIView.as_view(), name='retail-networks_create'),
     path('retail-networks/<int:pk>/', RetailNetworkDetailAPIView.as_view(), name='retail-networks_detail'),
